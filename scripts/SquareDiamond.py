071233866ad670f6e798fd4175c02b309cf17654
Johannes Zell <johannes@gifzly.de>
2019-12-11 12:52:11 +0100

    Updated SquareDiamond.py

--- scripts/SquareDiamond.py
+++ scripts/SquareDiamond.py
@@ -24,13 +24,6 @@ def convert_array_to_image(arr):
     return image
 
 def SquareDiamondAlgo(height, width):
-<<<<<<< HEAD
-=======
-    # The array must be square with edge length 2**n + 1
-    #n = 10
-    #N = 2**n + 1
->>>>>>> 95f6c81497c404deb331f4fb1d02972b25cbb235
-    
     # f scales the random numbers at each stage of the algorithm
     f = 1
 
