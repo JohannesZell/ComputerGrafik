a0a3919e08abaf5f4b7416b682f84c4b73cea46f
JohannesZell <40359929+JohannesZell@users.noreply.github.com>
2019-12-11 08:14:41 +0100

    Update Main.py

--- scripts/Main.py
+++ scripts/Main.py
@@ -3,8 +3,12 @@ import bpy
 DiamondSquare = bpy.data.texts['SquareDiamond.py'].as_module()
 PerlinNoise = bpy.data.texts['PerlinNoise.py'].as_module()
 
+#Width and height of the image
 Width = 1024
 Height = 1024
 
+#Create height map
 DiamondSquare.MainSquareDiamond(Height,Width)
+
+#Create Noise Map
 PerlinNoise.MainPerlinNoise(Height,Width)
